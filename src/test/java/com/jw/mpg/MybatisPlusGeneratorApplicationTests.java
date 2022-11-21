1e13fb06aaa8774cec598d9925cea641a66fd9f3
junkai.wang <junkai.wang@xinjifamily.com>
2022-11-21 10:11:58 +0800

    first commit

---
+++ src/test/java/com/jw/mpg/MybatisPlusGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.mpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
